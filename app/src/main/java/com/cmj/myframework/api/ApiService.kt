1c9f6f8216b6afaba7284e70577f1e55a33d2fab
彭付林 <pfl(*)2016>
2020-10-15 23:09:19 +0800

    1.Api封装调整。

--- app/src/main/java/com/cmj/myframework/api/ApiService.kt
+++ app/src/main/java/com/cmj/myframework/api/ApiService.kt
@@ -1,11 +1,8 @@
 package com.cmj.myframework.api
 
-open interface ApiService {
-    var asd: String?
-
+interface ApiService {
     companion object{
         const val  BASE_URL: String = "https://news-at.zhihu.com/api/4/"
         const val JUE_JIN_BASE_URL: String = "http://timeline-merger-ms.juejin.im/v1/"
     }
-
 }
