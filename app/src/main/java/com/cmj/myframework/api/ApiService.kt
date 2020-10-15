5bbd7a1c82006ee7af050dbbbe63b76987ad5a6e
彭付林 <pfl(*)2016>
2020-10-15 23:26:42 +0800

    1.Api封装调整。

--- app/src/main/java/com/cmj/myframework/api/ApiService.kt
+++ app/src/main/java/com/cmj/myframework/api/ApiService.kt
@@ -2,7 +2,7 @@ package com.cmj.myframework.api
 
 interface ApiService {
     companion object{
-        const val  BASE_URL: String = "https://news-at.zhihu.com/api/4/"
-        const val JUE_JIN_BASE_URL: String = "http://timeline-merger-ms.juejin.im/v1/"
+        const val  BASE_URL = "https://news-at.zhihu.com/api/4/"
+        const val JUE_JIN_BASE_URL = "http://timeline-merger-ms.juejin.im/v1/"
     }
 }
---
+++ app/src/main/java/com/cmj/myframework/api/Constants.kt
@@ -0,0 +1,9 @@
+package com.cmj.myframework.api
+
+enum class Constants(url : String) {
+    BASE_URL("https://news-at.zhihu.com/api/4/"),
+    JUE_JIN_BASE_URL("https://news-at.zhihu.com/api/4/"),
+    JUE_JIN_BASE_URL1("https://news-at.zhihu.com/api/4/"),
+    JUE_JIN_BASE_URL2("https://news-at.zhihu.com/api/4/"),
+    JUE_JIN_BASE_URL3("https://news-at.zhihu.com/api/4/"),
+}
