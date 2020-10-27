7a11b972b0cfa4424686b3d74ae08921724853b6
彭付林 <pfl(*)2016>
2020-10-27 17:57:55 +0800

    1.封装链式调用例子。

--- app/src/main/java/com/cmj/myframework/ui/test/MyTestViewModel.kt
+++ app/src/main/java/com/cmj/myframework/ui/test/MyTestViewModel.kt
@@ -4,10 +4,9 @@ import androidx.lifecycle.MutableLiveData
 import com.cmj.basiclib.lifecycle.BaseViewModel
 import com.cmj.myframework.api.Api
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 
-class MyTestViewModel : BaseViewModel(){
+class MyTestViewModel : BaseViewModel() {
 
     /**
      * 当数据请求成功回调
@@ -15,9 +14,17 @@ class MyTestViewModel : BaseViewModel(){
     protected var news = MutableLiveData<NewsBean>()
 
     public fun requstData() {
-        showDialog.setValue(true , "加载中")
-
+        showDialog.setValue(true, "加载中")
         var disposable = Api.getInstence().news()
+//            .flatMap(Function<NewsBean , ObservableSource<JueJinBean>>(){
+//                Api.getInstence().jueJin("","","")
+//            })
+            //            .flatMap {
+//                Api.getInstence().jueJin("","","")
+//            }
+//            .flatMap(Function {
+//                Api.getInstence().jueJin("","","")
+//            })
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
