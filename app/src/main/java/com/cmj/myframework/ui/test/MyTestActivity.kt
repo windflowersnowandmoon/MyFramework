2d3f1b722206763317cf607f070fa0d72737b04a
彭付林 <pfl(*)2016>
2020-11-02 22:59:52 +0800

    1.数据库调整。

--- app/src/main/java/com/cmj/myframework/ui/test/MyTestActivity.kt
+++ app/src/main/java/com/cmj/myframework/ui/test/MyTestActivity.kt
@@ -4,6 +4,8 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.cmj.basiclib.base.BaseActivity
+import com.cmj.commonlib.database.AppDatabase
+import com.cmj.commonlib.database.Menses
 import com.cmj.myframework.R
 import kotlinx.android.synthetic.main.activity_my_test.*
 
@@ -20,8 +22,20 @@ class MyTestActivity : BaseActivity<MyTestViewModel>() {
     }
 
     override fun initView() {
+       var mMensesDao = AppDatabase.getInstance(this).getMensesdDao()
+        var sList: MutableList<Menses> = mutableListOf<Menses>()
+
         btn_request_data.setOnClickListener {
             viewModel.requstData()
+        }
+
+        btn_test_db.setOnClickListener {
+            for (index in 1L..10L ){
+                var userId = 1 + index
+                var m1 = Menses(userId , "Menses1" , "成都1" , userId)
+                sList.add(m1)
+            }
+            mMensesDao.insertAll(sList)
         }
     }
 
--- commonLib/src/main/java/com/cmj/commonlib/database/AppDatabase.kt
+++ commonLib/src/main/java/com/cmj/commonlib/database/AppDatabase.kt
@@ -8,19 +8,24 @@ import androidx.room.RoomDatabase
 @Database(entities = [Menses::class , User::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
 
-    abstract fun mensesdDao() : Menses
+    abstract fun getMensesdDao() : MensesDao
 
-//    abstract fun
+    abstract fun getUserDao() : UserDao
 
     companion object {
         private var instance: AppDatabase? = null
         fun getInstance(context: Context): AppDatabase {
             if (instance == null) {
-                instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "user.db" //数据库名称
-                ).allowMainThreadQueries().build()
+                synchronized(AppDatabase::class) {
+                    if (instance != null){
+                        instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            AppDatabase::class.java,
+                            "cmj.db" //数据库名称
+                        ).allowMainThreadQueries().build()
+                    }
+                }
+
             }
             return instance as AppDatabase
         }
