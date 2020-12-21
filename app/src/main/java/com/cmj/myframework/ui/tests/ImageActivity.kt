da6c4e3be0dffaa6a009e1ca68402c14c30a2122
彭付林 <pfl(*)2016>
2020-12-21 22:21:13 +0800

    1.Glide测试提交

--- app/src/main/java/com/cmj/myframework/ui/tests/ImageActivity.kt
+++ app/src/main/java/com/cmj/myframework/ui/tests/ImageActivity.kt
@@ -2,14 +2,18 @@ package com.cmj.myframework.ui.tests
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.cmj.myframework.R
+import kotlinx.android.synthetic.main.activity_image.*
 
 class ImageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
-
+        Glide.with(this@ImageActivity)
+            .load("https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1608214933&di=2eb93fdd4634ec055eacd7abd1480760&src=http://a2.att.hudong.com/27/81/01200000194677136358818023076.jpg")
+            .into(test_glide)
     }
 
 }
